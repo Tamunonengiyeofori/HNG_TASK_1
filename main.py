fb309a347c65f2314c490a04ff2d176c7ed8178b
Tamunonengiyeofori <tkennwariso@gmail.com>
2022-10-27 15:13:35 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+details = {
+    "slackUsername": "Nengi_Tammy",
+    "backend": True,
+    "age": "21",
+    "bio": " I am an aspiring python developer who works with FatsAPI and Django"
+    
+}
+
+
+# class UserInfo(BaseModel):
+#     slackUsername : str
+#     backend : bool
+#     age: int
+#     bio : str   
+
+
+
+
+@app.get("/user_info")
+def get_details():
+    return details
+    
